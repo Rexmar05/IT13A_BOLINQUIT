00770c3330ae5c44d179874a416812a3b046d108
Rexmar05 <rekkussuma05@gmail.com>
2025-02-13 23:39:05 +0800

    Added HelloWorld class

---
+++ IT13A_BOLINQUIT_activities/src/Prelim/Lab_act1_Arithmetics.java
@@ -0,0 +1,27 @@
+
+package Prelim;
+
+
+public class Lab_act1_Arithmetics {
+  
+    
+    public static void main(String[] args){
+       
+       int k= 10;
+       int l= 4;
+       int m= 6;
+       
+       int first= k*l+m;
+       int second= (k-l)%m;
+       int third=(k+l+m)/3;
+       int fourth=k*m-(l*l);
+       
+       System.out.println("10 * 4+ 6  = " + first);
+       System.out.println("(10 - 4)%6 = " + second);
+       System.out.println("(10+4+6)/3 = " + third);
+       System.out.println("10 * 6-(4*4) = " + fourth);
+       
+        
+    }
+}
+
